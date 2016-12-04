e34979c81096cc95df9b5e0de2fcdb0d328908f6
ibibah <ibibahwal@gmail.com>
2016-12-04 13:35:14 +0100

    - probleme d'init ethernet :   - deplacement delay(2000) au debut de setup()   - plus gateway et subnet - ajout EthernetBonjour pour trouve ibibahjeedom et s'annoncer comme ibibahteleinfo

--- TeleInfo/TeleInfo.ino
+++ TeleInfo/TeleInfo.ino
@@ -22,7 +22,7 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
 #include <SoftwareSerial.h>
-
+#include <EthernetBonjour.h>    // https://github.com/TrippyLighting/EthernetBonjour
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -31,9 +31,12 @@ byte mac[] =
   0xED, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
 IPAddress ip(192, 168, 10, 251);
-IPAddress broadcastIp(192, 168, 10, 255);
+IPAddress gateway(192, 168, 10, 1);
+IPAddress subnet(255, 255, 255, 0);
 unsigned int localPort = 2200;      // local port to listen on
 
+IPAddress broadcastIp(192, 168, 10, 255);
+IPAddress remoteJeedomIp(0, 0, 0, 0);
 // buffers for receiving and sending data
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
 char  ReplyBuffer[] = "acknowledged";       // a string to send back
@@ -43,37 +46,102 @@ SoftwareSerial teleinfo(2, 3);
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
+const char* ip_to_str(const uint8_t*);
+void nameFound(const char* name, const byte ipAddr[4]);
 
 void setup()
 {
-
-  // start the Ethernet connection:
-  Ethernet.begin(mac, ip);
-    Udp.begin(localPort);
-
   // Open serial communications and wait for port to open:
   Serial.begin(1200);
   teleinfo.begin(1200);
-
+  
   // give the Ethernet shield a second to initialize:
   Serial.println("waiting ethernet shield initialisation...");
   delay(2000);
   Serial.println("OK.");
+  
+  // start the Ethernet connection:
+  Ethernet.begin(mac, ip, gateway, subnet);
+  Udp.begin(localPort);
+
+  // Initialize the Bonjour/MDNS library. You can now reach or ping this
+  // Arduino via the host name "arduino.local", provided that your operating
+  // system is Bonjour-enabled (such as MacOS X).
+  // Always call this before any other method!
+  EthernetBonjour.begin("ibibahteleinfo");
+
+  // We specify the function that the Bonjour library will call when it
+  // resolves a host name. In this case, we will call the function named
+  // "nameFound".
+  EthernetBonjour.setNameResolvedCallback(nameFound);
 }
 
 void loop()
 {
-  // as long as there are bytes in the serial queue,
-  // read them and send them out the socket if it's open:
-  while (teleinfo.available() > 0)
+  if (remoteJeedomIp == IPAddress(0,0,0,0) )
   {
-    char inChar = teleinfo.read() & 0x7F;
-    // send a reply, to the IP address and port that sent us the packet we received
-      Udp.beginPacket(broadcastIp, localPort);
+    if (EthernetBonjour.isResolvingName()== false) 
+    {
+      Serial.print("Resolving '");
+      Serial.print("ibibahjeedom");
+      Serial.println("' via Multicast DNS (Bonjour)...");
+
+      // Now we tell the Bonjour library to resolve the host name. We give it a
+      // timeout of 5 seconds (e.g. 5000 milliseconds) to find an answer. The
+      // library will automatically resend the query every second until it
+      // either receives an answer or your timeout is reached - In either case,
+      // the callback function you specified in setup() will be called.
+
+      EthernetBonjour.resolveName("ibibahjeedom", 5000);
+    }    
+  }
+  else
+  {
+    // as long as there are bytes in the serial queue,
+    // read them and send them out the socket if it's open:
+    while (teleinfo.available() > 0)
+    {
+      char inChar = teleinfo.read() & 0x7F;
+      // send a reply, to the IP address and port that sent us the packet we received
+      Udp.beginPacket(remoteJeedomIp, localPort);
       Udp.write(inChar);
       Udp.endPacket();
       Serial.print(inChar);
+    }
   }
+  
+  // This actually runs the Bonjour module. YOU HAVE TO CALL THIS PERIODICALLY,
+  // OR NOTHING WILL WORK! Preferably, call it once per loop().
+  EthernetBonjour.run();
 
-    delay(10);
+  delay(1);
+}
+
+// This function is called when a name is resolved via MDNS/Bonjour. We set
+// this up in the setup() function above. The name you give to this callback
+// function does not matter at all, but it must take exactly these arguments
+// (a const char*, which is the hostName you wanted resolved, and a const
+// byte[4], which contains the IP address of the host on success, or NULL if
+// the name resolution timed out).
+void nameFound(const char* name, const byte ipAddr[4])
+{
+  if (NULL != ipAddr) {
+    Serial.print("The IP address for '");
+    Serial.print(name);
+    Serial.print("' is ");
+    Serial.println(ip_to_str(ipAddr));
+    remoteJeedomIp = IPAddress(ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
+  } else {
+    Serial.print("Resolving '");
+    Serial.print(name);
+    Serial.println("' timed out.");
+  }
+}
+
+// This is just a little utility function to format an IP address as a string.
+const char* ip_to_str(const uint8_t* ipAddr)
+{
+  static char buf[16];
+  sprintf(buf, "%d.%d.%d.%d\0", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3]);
+  return buf;
 }
